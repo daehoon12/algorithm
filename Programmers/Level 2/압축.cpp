cf7a73d1f911b41027a318b35b091a7ec7da3c63
kang dae hoon <foreat_14@naver.com>
2020-07-12 00:01:47 +0900

    Algorithm

---
+++ Programmers/Level 2/압축.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iterator>
+using namespace std;
+
+void map_init(map<string, int> & m)
+{
+
+    for (int i = 0; i < 26; i++)
+    {
+        string ch = "";
+        ch+= 'A' + i;
+        m[ch] = i + 1;
+    }
+}
+
+bool check(string s, map<string, int>& m)
+{
+    for (auto iter = m.begin(); iter != m.end(); iter++)
+    {
+        if (iter->first == s)
+            return true;
+    }
+    return false;
+}
+vector<int> solution(string msg) {
+    vector<int> answer;
+    map<string, int > m;
+    map_init(m);
+    string temp = "";
+    int temp_answer = 0;
+    int idx = 27;
+    for (int i = 0; i< msg.size(); i++)
+    {
+        temp += msg[i];
+        if (check(temp, m))
+        {
+            temp_answer = m[temp];
+        }
+        else
+        {
+            answer.push_back(temp_answer);
+            m[temp] = idx++;
+            temp = "";
+            i--;
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    solution("KAKAO");
+    return 0;
+}
