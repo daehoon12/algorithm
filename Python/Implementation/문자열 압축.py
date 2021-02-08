82f18c59eca51a65b1f802415383eb18f8708da3
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2021-02-08 19:30:36 +0900

    Update 문자열 압축.py

--- Python/Implementation/문자열 압축.py
+++ Python/Implementation/문자열 압축.py
@@ -12,7 +12,7 @@ def solution(s):
             if s[i:i+cut] == tempStr: # 범위 내 길이가 같으면 count 증가 
                 count += 1
             else:
-                if count == 1: 
+                if count == 1: # 아닐시 1이면 1을 없애기 위해 count =""
                     count = ""
                 result += str(count) + tempStr 
                 tempStr = s[i:i+cut]
